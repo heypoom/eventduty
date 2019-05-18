a1a94b7fa4298d06cab47d24184240dcce1e98a1
Phoomparin Mano <phoomparin@gmail.com>
2019-05-19 00:44:59 +0700

    :sparkles: CSS Grid Experiments

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,5 +1,29 @@
 import React, {useEffect, useRef} from 'react'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  display: grid;
+  min-height: 100vh;
+
+  grid-template-columns: 1fr 1fr;
+`
+
+const Box = styled.div`
+  background: ${props => props.color};
+
+  color: #2d2d30;
+  font-size: 3em;
+  font-family: 'FuraCode Nerd Font', sans-serif;
+  padding: 1em;
+`
 
 export function App() {
-  return <div>Hello!</div>
+  return (
+    <Container>
+      <Box color="#50fa7b">Hello</Box>
+      <Box color="#8be9fd">World</Box>
+      <Box color="#50fa7b">Hello</Box>
+      <Box color="#8be9fd">World</Box>
+    </Container>
+  )
 }
