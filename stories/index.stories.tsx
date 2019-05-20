1abf9b47a0e863d9014123dd9a6df5fad6a23030
Phoomparin Mano <phoomparin@gmail.com>
2019-05-20 23:40:48 +0700

    :white_check_mark: Add countdown clock story

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import {storiesOf} from '@storybook/react'
+import styled from '@emotion/styled'
 
 import {useRemainingTime} from '../src/time-slot/use-remaining-time'
+import {getNextSlot} from '../src/time-slot/get-next-slot'
+
+const LargeText = styled.div`
+  color: #333;
+  font-family: 'FuraCode Nerd Font', 'Sukhumvit Set', sans-serif;
+  font-size: 4em;
+  padding: 0.5em;
+`
 
 const RemainingTimeDisplay = () => {
-  const remainingTime = useRemainingTime('12:30')
+  const d = new Date()
+  const ns = getNextSlot(d.getHours() + ':' + d.getMinutes())
+  const remainingTime = useRemainingTime(ns)
 
-  return <div>{remainingTime}</div>
+  return <LargeText>{remainingTime}</LargeText>
 }
 
-storiesOf('Get Remaining Time', module).add('basic', () => {
+storiesOf('Get Remaining Time', module).add('Countdown Clock', () => {
   return <RemainingTimeDisplay />
 })
